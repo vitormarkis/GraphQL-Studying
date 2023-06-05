5536fca19724218f2bfaeef3f2c786f2bf9d4ee4
Vitor Markis <vitormarkis2369@gmail.com>
2023-06-05 05:15:34 -0300

    feat: create server

---
+++ server/src/server.ts
@@ -0,0 +1,21 @@
+import { ApolloServer } from "apollo-server";
+import "reflect-metadata";
+import { buildSchema } from "type-graphql";
+import { AppointmentsResolver } from "./resolvers/appointments-resolver";
+import path from "node:path";
+
+async function bootstrap() {
+  const schema = await buildSchema({
+    resolvers: [AppointmentsResolver],
+    emitSchemaFile: path.resolve(__dirname, "schema.gql"),
+  });
+
+  const server = new ApolloServer({
+    schema,
+  });
+  const { url } = await server.listen();
+
+  console.log(`🎈 Server is running on ${url}`);
+}
+
+bootstrap();
